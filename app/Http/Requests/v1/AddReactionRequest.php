61444c45bf07e181a0947635d912f94fd56fa02f
gs-developer-omar <gs_zero_main@mail.ru>
2024-11-27 14:42:21 +0300

    Emoji validation rule

---
+++ app/Http/Requests/v1/AddReactionRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddReactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'emoji' => [
+                'required',
+                'string',
+                'regex:/^[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{1F700}-\x{1F77F}\x{1F780}-\x{1F7FF}\x{1F800}-\x{1F8FF}\x{1F900}-\x{1F9FF}\x{1FA00}-\x{1FA6F}\x{1FA70}-\x{1FAFF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{2300}-\x{23FF}\x{2B50}]+$/u',
+            ],
+        ];
+    }
+}
