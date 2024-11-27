642d3df08236bf938fc49886e4aa47f755602256
gs-developer-omar <gs_zero_main@mail.ru>
2024-11-27 15:02:59 +0300

    emoji +++++123++++

--- app/Http/Requests/v1/AddReactionRequest.php
+++ app/Http/Requests/v1/AddReactionRequest.php
@@ -25,7 +25,7 @@ class AddReactionRequest extends FormRequest
             'emoji' => [
                 'required',
                 'string',
-                'regex:/^(?:[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{1F700}-\x{1F77F}\x{1F780}-\x{1F7FF}\x{1F800}-\x{1F8FF}\x{1F900}-\x{1F9FF}\x{1FA00}-\x{1FA6F}\x{1FA70}-\x{1FAFF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{2300}-\x{23FF}\x{2B50}]+(?:\uFE0F|\u200D?)*|\p{So})$/u',
+                'regex:/^[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{1F700}-\x{1F77F}\x{1F780}-\x{1F7FF}\x{1F800}-\x{1F8FF}\x{1F900}-\x{1F9FF}\x{1FA00}-\x{1FA6F}\x{1FA70}-\x{1FAFF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{2300}-\x{23FF}\x{2B50}]$/u',
             ],
         ];
     }
